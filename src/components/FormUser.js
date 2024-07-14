48eef9bff6a06ebea4ef337e767f48e9e663c355
Andres David <kyfunky1386@gmail.com>
2024-07-14 17:24:52 -0500

    agregando usuario

---
+++ src/components/FormUser.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+import fetchPost from '../Methods/FetchPost';
+
+function FormUser({ show, handleClose }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+
+  const clearForm = () => {
+    setUsername('');
+    setPassword('');
+  };
+
+  const handleSubmit = async () => {
+    const userData = {
+      nit:username,
+      contrasena:password,
+    };
+
+    try {
+      const response = await fetchPost('https://mantenimientoautosbackend.onrender.com/user/register', userData);
+      if (response) {
+        handleClose();
+        clearForm();
+      } else {
+        alert('Error al crear el usuario.');
+      }
+    } catch (error) {
+      alert('Ocurrió un error al crear el usuario.');
+    }
+  };
+
+  const handleCloseModal = () => {
+    clearForm();
+    handleClose();
+  };
+
+  return (
+    <Modal show={show} onHide={handleCloseModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Crear Usuario</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="formUsername" className="mb-3">
+            <Form.Label>Nombre de Usuario</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el nombre de usuario"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="formPassword" className="mb-3">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Ingrese la contraseña"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleCloseModal}>
+          Cerrar
+        </Button>
+        <Button variant="primary" onClick={handleSubmit}>
+          Crear Usuario
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default FormUser;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,30 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Naipes from '../components/Naipes'; // Importa el componente Naipes desde su ubicación
 import { Button } from 'react-bootstrap'; // Importa el componente Button de react-bootstrap
 import { useNavigate } from 'react-router-dom'; // Importa el hook useNavigate de react-router-dom
-import { Col, Row } from "react-bootstrap"; // Importa los componentes Col y Row de react-bootstrap
+import { Col, Row, Container } from "react-bootstrap"; // Importa los componentes Col y Row de react-bootstrap
 import { AuthContext } from '../context/AuthContext';
-import { Container } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
+import FormUser from '../components/FormUser'; // Importa el componente FormUser desde su ubicación
 
 // Define el componente Home
-function Home () {
+function Home() {
     const navigate = useNavigate(); // Inicializa el hook useNavigate para la navegación
     const { isAdmin, handleLogout } = useContext(AuthContext);
-    const nombre = localStorage.getItem('nombre')
-    // Función para manejar el cierre de sesión
+    const nombre = localStorage.getItem('nombre');
+    
+    // Estados para manejar la visibilidad del modal
+    const [showModal, setShowModal] = useState(false);
 
+    // Función para manejar el cierre de sesión
     const handleLogoutClick = () => {
         handleLogout(); // Llama a la función handleLogout del contexto
         navigate('/'); // Navega a la página de inicio de sesión después de cerrar sesión
     };
 
+    // Función para manejar la apertura del modal
+    const handleShowModal = () => {
+        setShowModal(true);
+    };
+
+    // Función para manejar el cierre del modal
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
     return (
-        <Container>  
+        <Container>
             {/* Contenedor de la fila */}
             <Row>
                 {/* Columna para el botón de cierre de sesión */}
-                <Col sm={12} md={1} className="d-flex justify-content-center justify-content-md-end ">
+                <Col sm={12} md={1} className="d-flex justify-content-center justify-content-md-end">
                     {/* Botón para cerrar sesión */}
                     <Button variant="danger" onClick={handleLogoutClick} className="mt-3">
                         Cerrar
@@ -33,16 +46,31 @@ function Home () {
                 {/* Columna para mostrar si es administrador */}
                 <Col sm={12} md={2} className="d-flex justify-content-center">
                     {/* Condición para mostrar si es administrador */}
-                    {isAdmin ? <p className="fs-5 m-3 pt-2 fw-semibold text-wrap"><Image src="https://firebasestorage.googleapis.com/v0/b/images-6d562.appspot.com/o/images_mantenimiento%2Fusuario.png?alt=media&token=379901c5-3817-4531-9f69-ed5157a80ff1" alt='Logo' thumbnail  style={{ width: '35px', height: '35px', objectFit: 'cover' }} /> ({nombre})</p>:
-                    <p className="fs-4 m-3 pt-2 fw-semibold text-wrap"> <Image src="https://firebasestorage.googleapis.com/v0/b/images-6d562.appspot.com/o/images_mantenimiento%2Fusuario.png?alt=media&token=379901c5-3817-4531-9f69-ed5157a80ff1" alt='Logo' thumbnail  style={{ width: '35px', height: '35px', objectFit: 'cover' }} /> {nombre}</p>}
-
+                    {isAdmin ? (
+                        <p className="fs-5 m-3 pt-2 fw-semibold text-wrap">
+                            <Image src="https://firebasestorage.googleapis.com/v0/b/images-6d562.appspot.com/o/images_mantenimiento%2Fusuario.png?alt=media&token=379901c5-3817-4531-9f69-ed5157a80ff1" alt="Logo" thumbnail style={{ width: '35px', height: '35px', objectFit: 'cover' }} /> ({nombre})
+                        </p>
+                    ) : (
+                        <p className="fs-4 m-3 pt-2 fw-semibold text-wrap">
+                            <Image src="https://firebasestorage.googleapis.com/v0/b/images-6d562.appspot.com/o/images_mantenimiento%2Fusuario.png?alt=media&token=379901c5-3817-4531-9f69-ed5157a80ff1" alt="Logo" thumbnail style={{ width: '35px', height: '35px', objectFit: 'cover' }} /> {nombre}
+                        </p>
+                    )}
                 </Col>
+                {isAdmin ? (
+                <Col sm={12} md={9} className="d-flex justify-content-end">
+                    <Button variant="info" onClick={handleShowModal} className="mt-3">
+                        Crear Usuario
+                    </Button>
+                </Col>): null}
             </Row>
 
             {/* Componente Naipes con la URL proporcionada */}
-            <Naipes url="https://mantenimientoautosbackend.onrender.com/vehiculos/getAll"/>
+            <Naipes url="https://mantenimientoautosbackend.onrender.com/vehiculos/getAll" />
+
+            {/* Modal para crear usuario */}
+            <FormUser show={showModal} handleClose={handleCloseModal}/>
         </Container>
-    )
+    );
 }
 
-export default Home; // Exporta el componente Home por defecto
+export default Home;
